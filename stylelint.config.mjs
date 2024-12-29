18c019dbe00e502196b29b20bd5ed77580efac96
alpaca1231 <kazuki.amatatsu.fd@gmail.com>
2024-12-29 23:51:15 +0900

    chore: fix stlyelint config

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('stylelint').Config} */
 const config = {
   extends: ['stylelint-config-standard', 'stylelint-config-recess-order'],
+  rules: {
+    'selector-class-pattern': [
+      '^[a-z][a-zA-Z0-9]*$',
+      {
+        message: 'Expected class selector to be in camelCase',
+      },
+    ],
+  },
 };
 
 export default config;
