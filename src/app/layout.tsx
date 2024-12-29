af9189b1b57c4676515c196702e42eb3d22bdbf6
alpaca1231 <kazuki.amatatsu.fd@gmail.com>
2024-12-29 17:35:52 +0900

    chore: move to globals.css file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import '@acab/reset.css';
-import './globals.css';
+import '@/styles/globals.css';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
